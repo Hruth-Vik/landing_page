1fa94b222a139a7289e75512ce6497dc001670f0
HruthVik <hruthviknaik03@gmail.com>
2024-03-22 22:35:49 +0530

    started editing the first page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import Header from './Header/Header.jsx';
-
+import body_one from './body_one/body_one.jsx';
 function App() {
   return (
+    <>
     <Header/>
+    <body_one/>
+    </>
   );
 }
 
