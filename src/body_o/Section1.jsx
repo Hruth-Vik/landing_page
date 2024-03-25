1578a91cc575aec86a14fa8b67c35c3de485c8d0
HruthVik <hruthviknaik03@gmail.com>
2024-03-25 20:05:45 +0530

    edited some parts of the bottom page

---
+++ src/body_o/Section1.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./body_one.css";
+// import background from "./background.png";
+import icons from "./icon.png";
+
+const Section1 = () => {
+  return (
+    <>
+        <div id="icon">
+          <img src={icons} alt="" />
+        </div>
+      <div className="body">
+        <div id="wrapper">
+          <div id="back">
+            <div className="heading">
+              <h1>Newly Released Games</h1>
+            </div>
+          </div>
+          <div id="center">
+            <div className="buttons">
+              <button id="all_btn">All</button>
+              <button id="PS4_btn">PS4</button>
+              <button id="PS5_btn">PS5</button>
+              <button id="X_Box_btn">X-Box</button>
+            </div>
+          </div>
+          <div className="containers"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Section1;
