d65aa7c560a364532915b8201a3e74d5f74e85ea
HruthVik <hruthviknaik03@gmail.com>
2024-03-27 15:01:46 +0530

    edited the page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import Header from './Header/Header.jsx';
 import Section1 from './body_o/Section1.jsx';
-
+import Secound from './body_two/Secound.jsx';
 function App() {
   return (
     <>
     <Header/>
     <Section1/>
+    <Secound/>
     </>
   );
 }
---
+++ src/body_two/Secound.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./secound.css";
+const Secound = () => {
+  return (
+    <div className='Sub1'>
+      <div className='First_text'>
+            <div className='dash'></div>
+            <p>What we do</p>
+            <div className='dash'></div>
+      </div>
+      <div className="second_txt">
+
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Secound
