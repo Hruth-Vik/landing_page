c04a26c8de3567ab17b12109fdf181dcc123b098
HruthVik <hruthviknaik03@gmail.com>
2024-03-28 21:54:07 +0530

    Formatted the document to make it user readable

--- src/body_two/Secound.jsx
+++ src/body_two/Secound.jsx
@@ -5,28 +5,28 @@ const Secound = () => {
   return (
     <div className='Sub1'>
       <div className='First_text'>
-            <div className='dash'></div>
-            <p>What we do</p>
-            <div className='dash'></div>
+        <div className='dash'></div>
+        <p>What we do</p>
+        <div className='dash'></div>
       </div>
       <div className="second_txt">
-            <h1 id="sec_heading">Meeting the needs of players across the world</h1>
+        <h1 id="sec_heading">Meeting the needs of players across the world</h1>
       </div>
       <div className="third_div">
-            <div className='box_card'>
-                <div className='upper_box'>
-                    
-                </div>
-                <div className='mid_box'>
+        <div className='box_card'>
+          <div className='upper_box'>
 
-                </div>
-                <div className='mid_lower_box'>
+          </div>
+          <div className='mid_box'>
 
-                </div>
-                <div className='lower_box'>
-                  List of carefully selected,verified,high quality suppliers in different regions worldwide.
-                </div>
-            </div>
+          </div>
+          <div className='mid_lower_box'>
+
+          </div>
+          <div className='lower_box'>
+            List of carefully selected,verified,high quality suppliers in different regions worldwide.
+          </div>
+        </div>
       </div>
       <div></div>
     </div>
